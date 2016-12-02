37e51e934c5348a8b270433f3751cd58032e9407
Diego R. Brogna <dierbro@gmail.com>
2016-12-02 10:29:13 +0100

    Fix nj event

--- openstates/nj/events.py
+++ openstates/nj/events.py
@@ -40,7 +40,8 @@ class NJEventScraper(EventScraper, MDBMixin):
             'LSC': 'Legislative Services Commission',
             'THIE': 'Senate Task Force on Health Insurance Exchange Implementation Committee',
             'CASC': 'College Affordability Study Commission',
-            'CIR': 'State Commission of Investigation'
+            'CIR': 'State Commission of Investigation',
+            'TDWI': 'TDWI'
         }
         self._committees = overlay
 
